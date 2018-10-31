08b56dc2eebc627e2fe084805deaf3640ed2442a
Tim Matthews <timmy.a.matthews@gmail.com>
2018-10-31 16:25:34 -0600

    skills parallax implemented

---
+++ src/components/IconDisplay.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Motion, spring } from "react-motion";
+
+export default class IconDisplay extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      iconHover: false
+    };
+  }
+
+  render() {
+    return (
+      <Motion
+        initialStyle={{ iconOpacity: 1, titleOpacity: 0 }}
+        style={{
+          iconOpacity: this.state.iconHover ? spring(0.5) : spring(1),
+          titleOpacity: this.state.iconHover ? spring(0.75) : spring(0)
+        }}
+      >
+        {mot => {
+          return (
+            <div
+              onMouseEnter={() => this.setState({ iconHover: true })}
+              onMouseLeave={() => this.setState({ iconHover: false })}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <img
+                src={this.props.img}
+                alt={`${this.props.img} icon`}
+                style={{
+                  height: "150px",
+                  width: "150px",
+                  opacity: mot.iconOpacity
+                }}
+              />
+              <div
+                style={{
+                  textAlign: "center",
+                  opacity: mot.titleOpacity,
+                  fontSize: "25px"
+                }}
+              >
+                {this.props.title}
+              </div>
+            </div>
+          );
+        }}
+      </Motion>
+    );
+  }
+}
--- src/components/SkillsDisplay.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import { Parallax } from "react-parallax";
-
-export default class SkillsDisplay extends React.Component {
-  render() {
-    return <div style={{ height: "70vh", width: "100%" }}>Skills</div>;
-  }
-}
