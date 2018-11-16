890a74ea11b1888856dbb423f8a7b0c9ac1a23ec
Tim Matthews <timmy.a.matthews@gmail.com>
2018-11-15 20:20:23 -0700

    push

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + "./../public"));
+app.use(express.static(__dirname + "./../build"));
 app.listen(3013, () => console.log("listening on port 3013"));
